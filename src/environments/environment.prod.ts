749459e86924bfeab89760d5bff2812502f062b2
Sachin20010517 <sachinayeshmantha@gmail.com>
2026-05-24 11:16:24 +0100

    just updated the github workflow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: false,
-  serverUrl: 'https://imo-user-mgt.onrender.com/User-Mgt/api',
-  SupplierUrl: 'https://imo-mgt-be.onrender.com/Suppliers/api',
+  serverUrl: 'https://user-mgt.smartstockflow.co.uk/User-Mgt/api',
   ReportUrl: 'http://127.0.0.1:8886/Reports/api',
-   OrganizeUrl: 'http://127.0.0.1:8888/Organizations/api',
- InventoryUrl: 'http://127.0.0.1:8882/Inventory/api',
+  OrganizeUrl: 'http://127.0.0.1:8888/Organizations/api',
+ InventoryUrl: 'https://api.smartstockflow.co.uk/DemandFlow-Service/api',
+ forecasturl: 'https://forecast.smartstockflow.co.uk/api/v1/',
+ FileManagementUrl: 'https://filemgt.smartstockflow.co.uk/File-Mgt/api/CreateFile',
  
   LogoLight: '#',
   LogoDark: '#',
@@ -14,7 +15,7 @@ export const environment = {
     clientId: '56bSrJyNAAcsE7Hfs88lBH3BEXYesuwA',
     authorizationParams: {
       // audience: 'https://h-pos.us.auth0.com/api/v2/',
-      redirect_uri: 'https://imo-mgt-fe.onrender.com',
+      redirect_uri: 'https://smartstockflow.co.uk',
     },
   },
 };
